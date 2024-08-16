6a762ce529009c37957c98bc3503208dbb7c847f
lenny_cheng <langrisser1981@gmail.com>
2024-08-16 11:20:36 +0800

    [feat]: 初始化專案，移除storyboard
    內容:
    建立專案，移除內建的storyboard檔案，並指定一個預設的白色背景與開發中文字
    
    TODO:
    根據需求安排資料架構

---
+++ BankAppDemo/BankAppDemo/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  BankAppDemo
+//
+//  Created by 程信傑 on 2024/8/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 設置背景顏色為白色
+        view.backgroundColor = .white
+
+        // 創建label
+        let label = UILabel()
+        label.text = "開發中"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 24)
+
+        // 設置label的autoresizing屬性為false,以便使用Auto Layout
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        // 將label添加到view中
+        view.addSubview(label)
+
+        // 設置Auto Layout約束
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
