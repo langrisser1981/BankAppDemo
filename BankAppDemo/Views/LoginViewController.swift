e8971882fde953706a56f86d8ca06ebc21da5069
lenny_cheng <langrisser1981@gmail.com>
2024-08-17 19:43:39 +0800

    [feat] 修改登入頁面的按鈕排版，使用 stack
    內容:
    1. LoginVC: 將建立按鈕的方法獨立出來，並使用新建立的 Stack 排列按鈕

--- BankAppDemo/Views/LoginViewController.swift
+++ BankAppDemo/Views/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SnapKit
 import UIKit
 
 // MARK: - LoginViewControllerDelegate
@@ -18,51 +19,49 @@ protocol LoginViewControllerDelegate: AnyObject {
 
 class LoginViewController: UIViewController {
 	weak var delegate: LoginViewControllerDelegate?
-    
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		setupUI()
 	}
-    
+
 	private func setupUI() {
 		// 創建三個按鈕
-		let button1 = UIButton(type: .system)
-		button1.setTitle("狀態 1", for: .normal)
-		button1.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
-		button1.tag = 1
-        
-		let button2 = UIButton(type: .system)
-		button2.setTitle("狀態 2", for: .normal)
-		button2.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
-		button2.tag = 2
-        
-		let button3 = UIButton(type: .system)
-		button3.setTitle("狀態 3", for: .normal)
-		button3.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
-		button3.tag = 3
-        
-		// 設置按鈕的佈局
-		// 創建一個垂直堆疊視圖
-		let stackView = UIStackView(arrangedSubviews: [button1, button2, button3])
-		stackView.axis = .vertical
-		stackView.spacing = 20
-		stackView.alignment = .center
-		stackView.translatesAutoresizingMaskIntoConstraints = false
+		let button1 = createButton(title: "狀態 1", tag: 1)
+		let button2 = createButton(title: "狀態 2", tag: 2)
+		let button3 = createButton(title: "狀態 3", tag: 3)
 
-		// 將堆疊視圖添加到主視圖
+		// 創建一個垂直堆疊視圖
+		let stackView = UIStackView.create(
+			arrangedSubviews: [button1, button2, button3],
+			axis: .vertical,
+			spacing: 20,
+			alignment: .center
+		)
 		view.addSubview(stackView)
-        
-		// 設置堆疊視圖的約束
-		NSLayoutConstraint.activate([
-			stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-			button1.widthAnchor.constraint(equalToConstant: 200),
-			button2.widthAnchor.constraint(equalToConstant: 200),
-			button3.widthAnchor.constraint(equalToConstant: 200)
-		])
+
+		stackView.snp.makeConstraints { make in
+			make.center.equalToSuperview()
+		}
+
+		// 設置按鈕的佈局
+		for button in [button1, button2, button3] {
+			button.snp.makeConstraints { make in
+				make.width.equalTo(200)
+			}
+		}
 	}
-    
+
 	@objc private func buttonTapped(_ sender: UIButton) {
 		delegate?.didSelectStatus(sender.tag)
+	}
+
+	// 創建按鈕的輔助方法
+	private func createButton(title: String, tag: Int) -> UIButton {
+		let button = UIButton(type: .system)
+		button.setTitle(title, for: .normal)
+		button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+		button.tag = tag
+		return button
 	}
 }
