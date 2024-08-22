c080b2d02f6ba67d5815b9f2329c92858aaa8da1
lenny_cheng <langrisser1981@gmail.com>
2024-08-22 14:16:59 +0800

    [fix] 修正直接使用&指標存取(不安全存取)造成的報警
    內容:
    https://forums.swift.org/t/handling-the-new-forming-unsaferawpointer-warning/65523/6
    
    從swift5開始
    在 obj_getAssociatedObject | obj_setAssociatedObject
    直接使用 & 存取記憶體位址(指標存取)，系統會發出警告
    要改成使用UnsafeRawPoint(bitPattern:)來建立安全指標存取

--- BankAppDemo/Views/UILabel+Extension.swift
+++ BankAppDemo/Views/UILabel+Extension.swift
@@ -11,22 +11,22 @@ import UIKit
 extension UILabel {
 	// 用於儲存泡泡標籤的關聯物件鍵值
 	private enum AssociatedKeys {
-		static var bubbleLabelKey = "bubbleLabelKey"
+		static let bubbleLabelKey = UnsafeRawPointer(bitPattern: "bubbleLabelKey".hashValue)!
 	}
-    
+
 	// 泡泡標籤的私有屬性
 	private var bubbleLabel: UILabel? {
 		get {
-			objc_getAssociatedObject(self, &AssociatedKeys.bubbleLabelKey) as? UILabel
+			objc_getAssociatedObject(self, AssociatedKeys.bubbleLabelKey) as? UILabel
 		}
 		set {
-			objc_setAssociatedObject(self, &AssociatedKeys.bubbleLabelKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+			objc_setAssociatedObject(self, AssociatedKeys.bubbleLabelKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
 		}
 	}
-    
+
 	// 新增：定義泡泡標籤的最小尺寸
 	private static let minBubbleSize: CGFloat = 20
-    
+
 	/// 建立一個帶有泡泡數字的標籤
 	/// - Parameters:
 	///   - text: 主標籤的文字
@@ -37,7 +37,7 @@ extension UILabel {
 		label.text = text
 		label.textAlignment = .left // 將文字對齊改為左對齊
 		label.font = UIFont.systemFont(ofSize: 16)
-        
+
 		let bubbleLabel = UILabel()
 		bubbleLabel.textAlignment = .center
 		bubbleLabel.font = UIFont.systemFont(ofSize: 12)
@@ -49,11 +49,11 @@ extension UILabel {
 		// 移除外框
 		bubbleLabel.layer.borderWidth = 0
 		bubbleLabel.isHidden = bubbleNumber == 0
-        
+
 		label.addSubview(bubbleLabel)
 		label.bubbleLabel = bubbleLabel
 		label.setBubbleNumber(bubbleNumber)
-        
+
 		// 設定泡泡標籤的約束
 		bubbleLabel.snp.makeConstraints { make in
 			make.top.equalTo(label.snp.top).offset(-10)
@@ -61,32 +61,32 @@ extension UILabel {
 			make.height.equalTo(minBubbleSize)
 			make.width.greaterThanOrEqualTo(minBubbleSize)
 		}
-		
+
 		// 設定文字內容的約束
 		label.snp.makeConstraints { make in
 			make.height.greaterThanOrEqualTo(20)
 		}
-		
+
 		// 在佈局變更後更新泡泡位置
 		label.setNeedsLayout()
 		label.layoutIfNeeded()
-        
+
 		return label
 	}
-    
+
 	/// 設定泡泡中顯示的數字
 	/// - Parameter number: 要顯示的數字
 	func setBubbleNumber(_ number: Int) {
 		guard let bubbleLabel = bubbleLabel else { return }
-        
+
 		if number > 0 {
 			bubbleLabel.text = number > 99 ? "99+" : "\(number)"
 			bubbleLabel.isHidden = false
-            
+
 			// 計算所需的寬度
 			let textWidth = bubbleLabel.intrinsicContentSize.width
 			let requiredWidth = max(textWidth + 10, UILabel.minBubbleSize)
-			
+
 			// 更新泡泡標籤的寬度約束
 			bubbleLabel.snp.updateConstraints { make in
 				make.width.greaterThanOrEqualTo(requiredWidth) // 確保有足夠的寬度
@@ -94,7 +94,7 @@ extension UILabel {
 		} else {
 			bubbleLabel.isHidden = true
 		}
-		
+
 		// 強制更新佈局
 		setNeedsLayout()
 		layoutIfNeeded()
